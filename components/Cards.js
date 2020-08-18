1593514c4569c7f9b6c45caf0b80f665354a7f31
Brittany Canty <bcanty92@gmail.comgit config --global user.email:bcanty92@gmail.com>
2020-08-17 19:29:57 -0500

    updated questions

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,49 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+console.log(axios.get('https://lambda-times-backend.herokuapp.com/articles'))
+
+const cardContainer = document.querySelector('.cards-container')
+
+function Cards(obj){
+    //for loop, loops through all of the articles
+    for (const [key, value] of Object.entries(obj)){
+        //loops through all of the elements in the articles 
+        value.forEach(element => {
+            //create element
+            let card = document.createElement('div')
+            let headline = document.createElement('div')
+            let author = document.createElement('div')
+            let imgContainer = document.createElement('div')
+            let img = document.createElement('img')
+            let authorName = document.createElement('span')
+
+         
+
+            //create class list
+            card.classList.add('card')
+            headline.classList.add('headline')
+            author.classList.add('author')
+            imgContainer.classList.add('img-container')
+
+            //create text content
+            headline.textContent = element.headline;
+            img.src = element.authorPhoto;
+            authorName.textContent = 'By ' + element.authorName
+
+            //append
+            cardContainer.appendChild(card)
+            card.appendChild(headline)
+            card.appendChild(author)
+            author.appendChild(imgContainer)
+            imgContainer.appendChild(img)
+            author.appendChild(authorName)
+
+        });
+    }
+    
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(res => Cards(res.data.articles));
--- components/Header.js
+++ components/Header.js
@@ -10,5 +10,33 @@
 //
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
+const headerContainer = document.querySelector('.header-container')
 
-function Header() {}
+function Header() {
+
+    //create element
+    let header = document.createElement('div')
+    let date = document.createElement('span')
+    let t1 = document.createElement('h1')
+    let temp = document.createElement('span')
+
+    //class list
+    header.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+
+    //text content
+    date.textContent = "MARCH 28, 2019";
+    t1.textContent = 'Lambda Times';
+    temp.textContent = '98°';
+
+    //append
+    headerContainer.appendChild(header)
+    header.appendChild(date)
+    header.appendChild(t1)
+    header.appendChild(temp)
+
+    return header
+}
+
+Header()
